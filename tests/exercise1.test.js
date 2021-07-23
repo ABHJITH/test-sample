b731aa42e3b816d3a274a9f523a6a7f2ac236611
ABHIJITH <abhijithsurendran6@gmail.com>
2021-07-23 18:31:58 +0530

    First commit

---
+++ tests/exercise1.test.js
@@ -0,0 +1,22 @@
+const exercise1 = require('../exercise1')
+
+describe('fizzBuzz',()=>{
+    it('Should return fizzBuzz when the input is divisible by 3 and 5',()=>{
+        const result = exercise1.fizzBuzz(15)
+        expect(result).toBe('FizzBuzz')
+    })
+
+    it('Should return fizz when the input is only divisible by 3',()=>{
+        const result = exercise1.fizzBuzz(3)
+        expect(result).toBe('Fizz')
+    })
+
+    it('Should return Buzz when the input is only divisible by 5',()=>{
+        const result = exercise1.fizzBuzz(5)
+        expect(result).toBe('Buzz')
+    })
+    it('Should throw an error when the input is not a number',()=>{
+        
+        expect(()=>{exercise1.fizzBuzz('test')}).toThrow()
+    })
+})
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -24,3 +24,54 @@ describe('greet', ()=>{
         expect(result).toMatch(/Abhi/)
     })
 })
+
+describe('getCurrency',()=>{
+    it('Should return supported currencies',() =>{
+        const result = lib.getCurrencies();
+
+        // Too genaral
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+
+        // Too Specific
+        expect(result[0]).toBe('USD')
+        expect(result[1]).toBe('AUD')
+        expect(result[2]).toBe('EUR')
+        expect(result.length).toBe(3)
+
+        // Proper way
+        expect(result).toContain('USD')
+        expect(result).toContain('AUD')
+        expect(result).toContain('EUR')
+
+        // Ideal way
+        expect(result).toEqual(expect.arrayContaining(['EUR','AUD','USD']))
+    })
+})
+
+describe('getProduct',()=>{
+    it('Should return the product with given id', ()=>{
+        const result  = lib.getProduct(1);
+        // expect(result).toEqual({id:1, price:10});
+        expect(result).toMatchObject({id:1 ,price: 10});
+        expect(result).toHaveProperty('id',1)
+    })
+})
+
+describe('registerUser', ()=>{
+    it('Should throw an error if username is falsy',()=>{
+        
+    const args = [null, undefined, NaN, '', 0, false]
+
+    args.forEach(element => {
+        expect(()=>{lib.registerUser(element)}).toThrow()
+    });
+    })
+
+    it('Should return an userObj if a valid object is passed',()=>{
+        const result = lib.registerUser('Abhi');
+        expect(result).toMatchObject({username: 'Abhi'})
+        expect(result.id).toBeGreaterThan(0)
+    })
+})
+
